eb6e12e131a2137e7aba94bbce3f71a7892015f7
kinnaamf <dimoje.1337@gmail.com>
2024-11-30 00:37:45 +0200

    Added main.c and functions.h

---
+++ include/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void load_file(const char *filename);
+void analyze_data();
+void export_data(const char *filename);
+
+#endif 
+
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("welcome to console-sheetspread");
+    return 0;
+}
